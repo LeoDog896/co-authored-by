b2fa20e92542d4633764296f08a08b9ee695f011
Tristan F <LeoDog896@hotmail.com>
2023-03-29 14:14:15 +0000

    feat: initial query

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
+<script lang="ts">
+    import { fetchEmail } from "$lib/query.ts"
+    import { onMount } from "svelte";
+
+    let email = "";
+
+    onMount(() => {
+        fetchEmail().then((data) => {
+            email = data.email;
+            alert(email)
+        });
+    });
+</script>
+
+{email}
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
