6d67e6fbfd83908ea9ee3cf9c5e954debef96ad3
Tristan F <LeoDog896@hotmail.com>
2023-03-29 16:48:45 +0000

    docs: specify oauth app

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # co-authored-by
 generate co-authored-by descriptions for authors, while (attempting to) respect email privacy
+
+This uses an OAuth app, specifically for grabbing public information, as specified in the request.
--- src/routes/callback/+page.svelte
+++ src/routes/callback/+page.svelte
@@ -1,3 +1,10 @@
 <script lang="ts">
-    
+    import { onMount } from "svelte";
+
+    onMount(() => {
+        // get the query from the window url
+        const query = window.location.search.substring(1);
+
+        // 
+    })
 </script>
